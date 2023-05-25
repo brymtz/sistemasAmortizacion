27898f6e75889131bc368e9ec406f2dea29f685f
Paulo Martinez <paulomartinez1999@gmail.com>
2023-05-25 16:51:52 -0500

    arreglo de diseños

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include_once ("backend/models/interes.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://tresplazas.com/web/img/big_punto_de_venta.png" rel="shortcut icon">
     <title>Simulador de Créditos</title>
 
 </head>
